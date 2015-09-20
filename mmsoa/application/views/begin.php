2d065dc6702874ae078755e589a2882790bf9f89
jiajidi <jiajidi@163.com>
2015-09-20 20:32:59 +0800

    大量主页样式修改

--- mmsoa/application/views/begin.php
+++ mmsoa/application/views/begin.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,7 +7,6 @@
 	<meta name="author" content="webThemez.com">
 	<title>Agency</title>
 	<link rel="favicon" href="assets/images/favicon.png">
-	<link rel="stylesheet" media="screen" href="http://fonts.useso.com/css?family=Open+Sans:300,400,700">
 	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
 	<link rel="stylesheet" href="assets/css/font-awesome.min.css"> 
 	<link rel="stylesheet" href="assets/css/bootstrap-theme.css" media="screen"> 
@@ -20,22 +19,22 @@
 	<![endif]-->
 </head>
 <body>
+    <div class="logo">
+        <a href="index.html"><img src="assets/images/150.png" width="150" align="center"></a>
+    </div>
 	<!-- Fixed navbar -->
-  <div class="logo">
-    <a href="index.html"><img src="assets/images/Awsome-agency-logo5.png" width="300" align="center"></a>
-  </div>
 	<div class="navbar navbar-inverse">
 		<div class="container">
+
 			<div class="navbar-header">
 				<!-- Button for smallest screens -->
 				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
-				
 			</div>
 			<div class="navbar-collapse collapse">
 				<ul class="nav navbar-nav  mainNav">
 					<li class="active"><a href="index.html">Home</a></li>
-					<li><a href="about.html">About</a></li>
-					<li><a href="testimonial.html">Testimonial</a></li>
+					<li><a href="about.html">Lost&amp;Found</a></li>
+					<li><a href="testimonial.html">Information</a></li>
 					<li><a href="contact.html">Contact</a></li>
 
 				</ul>
@@ -49,8 +48,8 @@
 	<header id="head">
 		<div class="container">
              <div class="heading-text">							
-							<h1 class="animated flipInY delay1">Falling Behind Google</h1>
-              <button class="infos-button">Get Informations</button>
+							<h1 class="animated flipInY delay1">中山大学东校区多媒体管理系统</h1>
+              <button class="infos-button">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;登陆&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</button>
 						</div>
             
 			       <div class="fluid_container">                       
@@ -68,93 +67,48 @@
       	 </header>
 	<!-- /Header -->
     <section class="main-contents">
-      <h2 class="text-center sub-texts">One mission. Help your business reach peak profits.</h2>
+      <h2 class="text-center sub-texts">Welcome to MOA</h2>
       <div class="container">
       <hr>
          <div class="item-section col-md-4 col-sm-4 col-xs-12">
             <ul>
               <li class="inline-text">
-                <i class="fa fa-star"></i><p class="icon-text">A visit back to The Vedas</p>
+                <i class="fa fa-star"></i><p class="icon-text">失物招领信息</p>
               </li>
-              <li class="inline-text">
-                <i class="fa fa-star"></i><p class="icon-text">A visit back to The Vedas</p>
-              </li> 
-              <li class="inline-text">
-                <i class="fa fa-star"></i><p class="icon-text">A visit back to The Vedas</p>
-              </li> 
-
             </ul>  
          </div>
          <div class="item-section col-md-4 col-sm-4 col-xs-12">
             <ul>
               <li class="inline-text">
-                <i class="fa fa-star"></i><p class="icon-text">A visit back to The Vedas</p>
+                <i class="fa fa-star"></i><p class="icon-text">多媒体服务信息</p>
               </li>
-              <li class="inline-text">
-                <i class="fa fa-star"></i><p class="icon-text">A visit back to The Vedas</p>
-              </li> 
-              <li class="inline-text">
-                <i class="fa fa-star"></i><p class="icon-text">A visit back to The Vedas</p>
-              </li> 
-
             </ul>  
          </div>
          <div class="item-section col-md-4 col-sm-4 col-xs-12">
             <ul>
               <li class="inline-text">
-                <i class="fa fa-star"></i><p class="icon-text">A visit back to The Vedas</p>
+                <i class="fa fa-star"></i><p class="icon-text">联系我们</p>
               </li>
-              <li class="inline-text">
-                <i class="fa fa-star"></i><p class="icon-text">A visit back to The Vedas</p>
-              </li> 
-              <li class="inline-text">
-                <i class="fa fa-star"></i><p class="icon-text">A visit back to The Vedas</p>
-              </li> 
             </ul>  
          </div>
      </div>
   </section>
-  
-  <div class="copyrights">Collect from <a href="http://www.cssmoban.com/" >网页模板</a></div>
 
-  <section class="call-to-action text-center">
-    <div class="text-all">
-      <h2 class="title">Theme Wagon Advisors</h2>
-      <h3 class="subtitle">Make sure your business succeeds in 2015.</h3>
-      <button class="infos-button">Get Informations</button>
-    </div>  
-  </section> 
+
+
   <footer>
-    <div class="footer-section ">
+    <div class="footer-bottom">
       <div class="container">
         <div class="social pull-left">
-          <p><i class="fa fa-phone"></i>541-846-8671 <i class="fa fa-envelope-o"></i>Support@MovingMountainsAdvisors.com</p>
+          <p><i class="fa fa-phone"></i>内线&nbsp;801&nbsp;/&nbsp;802<i class="fa fa-envelope-o"></i>rinkako@homu.me</p>
         </div>
         <div class="navigation">
           <ul class="pull-right">
-            <li><a href="index.html">Home</a></li>
-            <li><a href="about.html">About</a></li>
-            <li><a href="testimonial.html">Testimonial</a></li>
-            <li><a href="contact.html">Contact</a></li>
+            <li>Powered by MMS OA System</a></li>
           </ul>
         </div> 
       </div>
     </div>
-    <div class="footer-bottom">
-      <div class="container">
-        <div class="social pull-left">
-          <p>Designed by Theme Wagon Advisors | More Templates <a href="http://www.cssmoban.com/" target="_blank" title="模板之家">模板之家</a> - Collect from <a href="http://www.cssmoban.com/" title="网页模板" target="_blank">网页模板</a></p>
-        </div>
-        <div class="navigation">
-          <ul class="pull-right">
-              <li><a href="#"> <i class="fa fa-2x fa-facebook"></i></a></li>
-              <li><a href="#"><i class="fa fa-2x fa-twitter"></i></a></li>
-              <li><a href="#"><i class="fa fa-2x fa-pinterest"></i></a></li>
-            </ul>
-        </div> 
-      </div>
-    </div>     
-  </footer>
 
 	<!-- JavaScript libs are placed at the end of the document so the pages load faster -->
     <script src="assets/js/modernizr-latest.js"></script> 
@@ -170,7 +124,7 @@
 			jQuery('#camera_wrap_4').camera({
                 transPeriod: 500,
                 time: 3000,
-				height: '600',
+				height: '530',
 				loader: 'false',
 				pagination: true,
 				thumbnails: false,
