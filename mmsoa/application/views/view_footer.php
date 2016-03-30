0d7d0c8477c0aab95b48e44e60aa4c731bdbaf75
yangyunguo <13680956981@163.com>
2016-03-30 13:40:56 +0800

    nav优化 固定

--- mmsoa/application/views/view_footer.php
+++
@@ -1,8 +0,0 @@
-<div class="footer">
-	<div class="pull-right">
-		By：<strong>Wico &amp Rinkako</strong>
-	</div>
-	<div>
-		<strong>Copyright</strong> MOA &copy; 2016
-	</div>
-</div>
--- mmsoa/assets/js/searchuser.js
+++
@@ -1,7 +0,0 @@
-/**
- * 查看用户列表
- */
-function showMore(evt){
-  var obj = window.event?event.srcElement:evt.target;
-  document.cookie = "index="+ obj.value;
-}
